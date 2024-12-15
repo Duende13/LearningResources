f000453cbf045f72df4b52d1208c3d44a7a76d45
duende13 <susana.r.cunado@gmail.com>
2024-12-15 20:25:41 +0100

    Custom attribute directive SafeLink

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, input, inject } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -9,13 +9,20 @@ import { Directive } from '@angular/core';
 })
 export class SafeLinkDirective {
 
+  queryParam = input<string>('myapp', {alias:'appSafeLink'});
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() { 
     console.log('SafeLinkDirective is active!');
   }
 
   onConfirmLeavePage(event: MouseEvent){
     const wantsToLeave = window.confirm('Do you want to leave the app?');
+
     if(wantsToLeave) {
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
+      
       return;
     } 
     event?.preventDefault();
